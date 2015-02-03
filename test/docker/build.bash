58fff50e9f06fa4b36759cc5fb21110fbe8eae82
redcap97 <redcap97@gmail.com>
2015-02-03 23:29:41 +0900

    added build check on docker

---
+++ test/docker/build.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd /tmp/
+apt-get update
+apt-get upgrade
+apt-get install -y git build-essential pkg-config fuse libfuse-dev
+
+git clone -b ${BRANCH:-master} https://github.com/redcap97/fuse-mfs.git
+pushd fuse-mfs/
+make
+make install PREFIX=/usr
+popd
+
+dd if=/dev/zero of=disk.img count=$((1024*16)) bs=1024
+mkfs.mfs disk.img
+mkdir disk
+fuse-mfs -o allow_other,default_permissions,use_ino,attr_timeout=0 disk.img disk
+fusermount -u disk
+fsck.mfs disk.img
